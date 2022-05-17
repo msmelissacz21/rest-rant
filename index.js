26ff3b07cdb93e440305bd844c0d1dfc8bb96a7a
Melissa Zuengler <melissazuengler@Melissas-MacBook-Pro.local>
2022-05-16 23:04:59 -0500

    Adding remaining stub routes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.use('/places', require('./controllers/places))
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
@@ -13,3 +13,4 @@ app.get('*', (req, res) => {
 })
 
 app.listen(process.env.PORT)
+
