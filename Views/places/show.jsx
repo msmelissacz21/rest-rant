863bfc6010e79518474d635117044e31e5bfd46d
Melissa Zuengler <melissazuengler@Melissas-MacBook-Pro.local>
2022-06-07 21:09:59 -0500

    Added comments

--- Views/places/show.jsx
+++ Views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className="border">
+                    <h2 className="rant">{c.rant ? 'Rant!' : 'Rave! '}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
           <main>
@@ -21,10 +40,8 @@ function show (data) {
                     Serving {data.place.cuisines}
                 </h4>
   
-                <h2>Rating</h2>
-                <h5>Currently Unrated</h5>
                 <h2>Comments</h2>
-                <h6>No comments yet</h6>
+                {comments}
                 
                 <div className='row container-fluid'>
                     <a href={`/places/${data.id}/edit`} className='btn btn-warning col-sm-3'>
